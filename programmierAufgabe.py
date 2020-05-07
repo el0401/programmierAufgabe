1db05d1cecfe458700fae6ba387770c6fe1a7e9b
el0401 <elimani0411@gmail.com>
2020-05-08 00:14:06 +0200

    Second Primer added

--- programmierAufgabe.py
+++ programmierAufgabe.py
@@ -1,4 +1,4 @@
-def findsequence(path_file, length, mintemp, maxtemp, distance):
+def find_sequence(path_file, length, mintemp, maxtemp, distance):
     with open(path_file, 'r') as file:
         data = file.read().replace('\n', '')
 
@@ -13,43 +13,91 @@ def findsequence(path_file, length, mintemp, maxtemp, distance):
     primer2found = False
     primersfound = primer1found & primer2found
     acceptedlist = ['A', 'T', 'G', 'C']
-    start = 0
     primer1 = []
+    primer2 = []
 
-    while not primer1found:
+    while not primersfound:
+        start = 0
+        while not primer1found:
+            for x in range(start, totalnumb, 1):
+                if datalist[x] in acceptedlist:
+                    primer1.append(datalist[x])
 
-        for x in range(start, totalnumb, 1):
-            if datalist[x] in acceptedlist:
-                primer1.append(datalist[x])
+                    if len(primer1) == length:
+                        temp = temperature(primer1)
+                        if (temp >= mintemp) and (temp <= maxtemp):
+                            primer1found = True
+                            break
+                        else:
+                            primer1.clear()
+                            start = start + 1
+                            break
+                else:
+                    primer1.clear()
+                    start = start + 1
+                    break
 
-                if len(primer1) == length:
-                    temp = temperature(primer1)
-                    if (temp >= mintemp) and (temp <= maxtemp):
-                        primer1found = True
-                        break
-                    else:
-                        primer1.clear()
-                        start = start + 1
-                        break
-            else:
+            if totalnumb - start - distance - 2 * length <= 0:
                 primer1.clear()
-                start = start + 1
+                print('Keine Primerpaare gefunden')
+                primer1found = False
                 break
 
-        if totalnumb - start - distance - 2 * length <= 0:
-            primer1.clear()
-            print('Keine Primerpaare gefunden')
-            primersfound = False
+        if not primer1found:
             break
-    print(primer1)
+
+        start2 = start + distance
+        while not primer2found:
+            for x in range(start2, totalnumb, 1):
+                if datalist[x] in acceptedlist:
+                    primer2.append(datalist[x])
+
+                    if len(primer2) == length:
+                        temp2 = temperature(primer2)
+                        if (temp2 >= mintemp) and (temp2 <= maxtemp):
+                            primer2found = True
+                            break
+                        else:
+                            primer2.clear()
+                            start2 = start2 + 1
+                            break
+                else:
+                    primer2.clear()
+                    start2 = start2 + 1
+                    break
+
+            if totalnumb - start2 - length <= 0:
+                primer2.clear()
+                print('Keine Primerpaare gefunden')
+                primer2found = False
+                break
+
+        if not primer2found:
+            break
+        else:
+            primersfound = True
+
+    print("Primer 1: "+''.join(primer1)+" an Position "+start+", Schmelztemperatur: "+temp)
+    print(reverse_complement(primer2))
 
 
 def temperature(primer):
-    counterAT = 0
-    counterGC = 0
+    counter_at = 0
+    counter_gc = 0
     for x in primer:
         if (x == 'A') or (x == "T"):
-            counterAT += 1
+            counter_at += 1
         if (x == 'G') or (x == "C"):
-            counterGC += 1
-    return (2 * counterAT) + (4 * counterGC)
+            counter_gc += 1
+    return (2 * counter_at) + (4 * counter_gc)
+
+
+def reverse_complement(primer):
+    complement_dict = {"A": "T", "T": "A", "G": "C", "C": "G"}
+    count = 0
+    for x in primer:
+        primer[count] = complement_dict[x]
+        count += 1
+
+    return list(reversed(primer))
+
